51515f4f3081623c0fcfafd72a54b907468f002e
Anto2134 <129270289+Anto2134@users.noreply.github.com>
2023-05-14 16:18:12 +0200

    Add files via upload

---
+++ src test/it/uniroma3/diadia/StanzaTest.java
@@ -0,0 +1,57 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class StanzaTest {
+	Stanza s = new Stanza("stanzaTest");
+	Stanza s2 = new Stanza("stanzaTest2");
+	Attrezzo osso;
+	Attrezzo lanterna;
+
+	@Before
+	public void setUp() {
+		osso = new Attrezzo("osso",1);
+		lanterna = new Attrezzo("lanterna",3);
+	}
+
+	@Test
+	public void testAddAttrezzo() {
+		assertTrue(s.addAttrezzo(lanterna));
+
+	}
+	@Test
+	public void testGetStanzaAdiacente() {
+		assertNull(s.getStanzaAdiacente("nord"));
+	}
+	@Test
+	public void testSetStanzaAdiacente() {
+		s.impostaStanzaAdiacente("sud", s2);
+		assertEquals(s2,s.getStanzaAdiacente("sud"));
+	}
+	@Test
+	public void testGetAttrezzo() {
+		s.addAttrezzo(lanterna);
+		assertEquals(lanterna,s.getAttrezzo(lanterna.getNome()));
+	}
+	@Test
+	public void testRemoveAttrezzo() {
+		s.addAttrezzo(lanterna);
+		assertTrue(s.removeAttrezzo(lanterna));
+	}
+	@Test
+	public void testHasAttrezzo() {
+		s.addAttrezzo(lanterna);
+		assertTrue(s.hasAttrezzo(lanterna.getNome()));
+	}
+	@Test
+	public void testGetDirezioni() {
+		s.impostaStanzaAdiacente("sud", s2);
+		assertNotNull(s.getDirezioni());
+	}
+}
